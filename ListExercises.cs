6918309f52fb31269da168c54241d38ce521f055
DenisaMelindaKuncik <56263465+DenisaMelindaKuncik@users.noreply.github.com>
2019-11-29 10:03:36 +0200

    Added CapitalizeListElements method (#1)
    * Added CapitalizeListElements method
    
    * Added ReturnElementsStartingWithVowels and ReturnElementsStartingWithConsonant methods

---
+++ ListExercises.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomationStudyGit
+{
+    public class ListExercises
+    {
+        public List<string> CapitalizeListElements(List<string> list)
+        {
+            List<string> list1 = new List<string>(list);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                list1[i] = list1[i].ToLower();
+                list1[i] = CapitalizeString(list1[i]);
+            }
+            return list1;
+        }
+
+        private string CapitalizeString(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+
+        public List<string> ReturnElementsStartingWithVowels(List<string> list)
+        {
+            List<string> list1 = new List<string>(list);
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            for (int i = 0; i < list1.Count; i++)
+            {
+                char firstChar = list1[i].ToLower().ToCharArray()[0];
+                if (!vowels.Contains(firstChar))
+                {
+                    list1.RemoveAt(i);
+                    i--;
+                }
+            }
+            return list1;
+        }
+
+        public List<string> ReturnElementsStartingWithConsonant(List<string> list)
+        {
+            List<string> list1 = new List<string>(list);
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            for (int i = 0; i < list1.Count; i++)
+            {
+                char firstChar = list1[i].ToLower().ToCharArray()[0];
+                if (vowels.Contains(firstChar))
+                {
+                    list1.RemoveAt(i);
+                    i--;
+                }
+            }
+            return list1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AutomationStudyGit
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ListExercises ls = new ListExercises();
 
+            List<string> names = new List<string>() { "anA", "elena", "MARIA", "cRisTiAn", "aDriANA" };
+            List<string> capitalizedNames = ls.CapitalizeListElements(names);
+
+            Console.WriteLine("List with names capitalized:");
+            foreach (var elem in capitalizedNames)
+            {
+                Console.WriteLine(elem);
+            }
+
+            List<string> namesStartingWithVowels = ls.ReturnElementsStartingWithVowels(names);
+
+            Console.WriteLine("List with names starting with vowels:");
+            foreach (var elem in namesStartingWithVowels)
+            {
+                Console.WriteLine(elem);
+            }
+
+
+            List<string> namesStartingWithConsonant = ls.ReturnElementsStartingWithConsonant(names);
+
+            Console.WriteLine("List with names starting with consonant:");
+            foreach (var elem in namesStartingWithConsonant)
+            {
+                Console.WriteLine(elem);
+            }
         }
     }
 }
