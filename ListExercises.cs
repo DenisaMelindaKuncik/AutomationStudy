059762728fdd3dfe8f0b843cb763c199ff23bfc9
Denisa Sut <melinda.sut@blackline.com>
2019-11-29 09:37:36 +0200

    Added CapitalizeListElements method

---
+++ ListExercises.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AutomationStudyGit
+{
+    public class ListExercises
+    {
+        public List<string> CapitalizeListElements(List<string> list)
+        {
+            List<string> list1 = new List<string>(list);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                list1[i] = list1[i].ToLower();
+                list1[i] = CapitalizeString(list1[i]);
+            }
+            return list1;
+        }
+
+        private string CapitalizeString(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AutomationStudyGit
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ListExercises ls = new ListExercises();
 
+            List<string> names = new List<string>() { "anA", "elena", "MARIA", "cRisTiAn", "aDriANA" };
+            List<string> capitalizedNames = ls.CapitalizeListElements(names);
+
+            Console.WriteLine("List with names capitalized:");
+            foreach (var elem in capitalizedNames)
+            {
+                Console.WriteLine(elem);
+            }
         }
     }
 }
